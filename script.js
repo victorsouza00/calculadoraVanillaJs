c0f6a4ad712760d510e7a9c1ef7f226261ca8cc6
victorssalves <victorsouza183@gmail.com>
2022-04-06 15:24:11 -0300

    Estudando POO e pondo em pratica

---
+++ script.js
@@ -0,0 +1,53 @@
+class Calculator {
+    constructor(previusOperandTextElement , currentOperandTextElement ){
+        this.previusOperandTextElement = previusOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ""
+        this.previusOperand = ""
+        this.operation = undefined
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+        this.currentOperand = number
+
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
+
+
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationButtons = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const allClearButtons = document.querySelector("[data-all-clear]")
+const previusOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[data-current-operand")
+
+
+const calculator = new Calculator(previusOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener("click",() => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
